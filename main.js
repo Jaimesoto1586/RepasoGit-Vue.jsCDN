ec4e8b5998b3faf9a57da14d729c53e18567b970
Jaime <sotojaimeantonio@gmail.com>
2020-08-10 19:36:48 -0400

    Tercer commit de esta pagina

--- main.js
+++ main.js
@@ -3,44 +3,57 @@ var app = new Vue({
     data: {
         description: 'Lorem ipsus',
         product: 'Longaniza de Brocolli',
-        img: './assets/1.jpg',
+        selectedVariant: {},
+        // img: './assets/1.jpg',
         link: './longanizas.html',
-        stock: 12,
+        // stock: 12,
         details: ["5oo gr", "Organica", "Libre de Colesterol"],
         // AGREGGANDO OBJETOS
         variants: [{
             id: 5,
             type: 'Roja',
-            img: './assets/1.jpg'
+            img: './assets/1.jpg',
+            stock: 10,
+            default: true
         },
         {
             id: 4,
             type: 'Blanca',
             img: './assets/2.png',
+            stock: 0
         }
         ],
         // VAMOS A AGREGAR BOTONES
         cart: 0
     },
     methods: {
-        updateImg(img) {
-            this.img = img;
+        updateProduct(variant) {
+            this.selectedVariant = variant;
         },
         addToCart() {
             this.cart += 1
-            this.stock -= 1
+            this.selectedVariant.stock -= 1
         },
         removeFromCart() {
             if (this.cart > 0) {
                 // debugger pone en pausa la pagina en donde queramos hay que poner el debugger
                 this.cart -= 1
-                this.stock += 1
+                this.selectedVariant.stock += 1;
             }
         }
     },
     computed: {
         inStock(){
             return this.stock > 0 ? true : false //retorna el stock >0 ? (if) es verdadero : (else) es falso
+        },
+        img(){
+            return this.selectedVariant.img
+        },
+        stock(){
+            return this.selectedVariant.stock
         }
+    },
+    created() {
+        this.selectedVariant = this.variants.find(item => item.default == true)        
     },
 })
